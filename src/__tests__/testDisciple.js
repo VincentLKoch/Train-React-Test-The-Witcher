759fe63634f7a6d9d554553899de3dc7baac45a3
VincentLKoch <vincent.lel.koch@gmail.com>
2019-10-22 03:55:05 +0200

    Finish disciple tests

--- src/__tests__/testDisciple.js
+++ src/__tests__/testDisciple.js
@@ -6,19 +6,66 @@ describe('Disciple functions', () => {
             [
                 ['Ciri', 1, true],
                 ['Geralt', 3, true],
-                ['Jean-Louis', 4, false]
+                ['Jean-Louis', 4, false],
+                ['Jean-Roger', 10, false]
             ])(
-                'Tests %s with mushroom n°%i',
-                (name, numberOfMushroom, isAlive) => {
+                '%s eat mushroom n°%i',
+                (name, numberOfMushroom, isAliveExpected) => {
+                    console.log = jest.fn();
+
                     let testDisciple = new Disciple(name)
                     testDisciple.mushroomsAte = numberOfMushroom - 1
 
                     testDisciple.eatMushrooms()
 
                     const actualResult = testDisciple.isStillAlive
-                    expect(actualResult).toBe(isAlive)
+                    expect(actualResult).toBe(isAliveExpected)
                     expect(testDisciple.mushroomsAte).toBe(numberOfMushroom)
+
+                    if (!isAliveExpected) {
+                        expect(console.log).toHaveBeenCalledWith(name + ' screams and dies in agony.')
+                    }
                 }
             )
     })
+
+    describe('disciple training incrementation', () => {
+        it.each(
+            [
+                ['Ciri', 1],
+                ['Geralt', 2999]
+            ])(
+                'Training %s session n°%i',
+                (name, TrainingSession, ) => {
+                    let testDisciple = new Disciple(name)
+                    testDisciple.trainingSessions = TrainingSession - 1
+
+                    testDisciple.train()
+
+                    expect(testDisciple.trainingSessions).toBe(TrainingSession)
+                }
+            )
+    })
+
+    describe('disciple have been trained', () => {
+        it.each(
+            [
+                ['Ciri', 1, 1, true],
+                ['Geralt', 3, 3000, true],
+                ['Jean-Eude', 0, 2, false],
+                ['Jean Guillemin', 2, 0, false],
+                ['Jean-Gabriel', 0, 0, false]
+            ])(
+                'Is %s trained ? Already ate %i mushroom and did %i training session. Spoiler: %s',
+                (name, mushroomUsed, trainingDone, isTrainedExpected) => {
+                    let testDisciple = new Disciple(name)
+                    testDisciple.mushroomsAte = mushroomUsed
+                    testDisciple.trainingSessions = trainingDone
+
+                    expect(testDisciple.isTrained()).toBe(isTrainedExpected)
+                }
+            )
+    })
+
+
 })
