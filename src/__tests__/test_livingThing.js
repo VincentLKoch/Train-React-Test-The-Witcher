99be8d01558ee22b3ae2998405ef56a76b46010f
VincentLKoch <vincent.lel.koch@gmail.com>
2019-10-31 19:41:02 +0100

    test fight to death and clean fight test

--- src/__tests__/test_livingThing.js
+++ src/__tests__/test_livingThing.js
@@ -42,81 +42,70 @@ describe('Living things tests', () => {
                 })
     })
 
+    describe('Happy death fight', () => {
+        it.each(
+            [
+                ['Baggie The Clown', 'Chuck Norris', 5, 9999], //defender win
+                ['Lex', 'Robin', 30, 12], //attacker win
+                ['Crones', 'Robin', 31, 13], //attacker win ( uneven number )
+
+            ])('%s fight to Death %s',
+                (attackerName, defenderName, attackerHealth, defenderHealth) => {
+                    console.log = jest.fn();
+                    const attacker = new LivingThing(attackerName, 'steel', attackerHealth)
+                    const defender = new LivingThing(defenderName, 'steel', defenderHealth)
+
+                    attacker.fightToDeath(defender)
+
+
+                    if (!attacker.isAlive) { //defender won
+                        expect(console.log).toHaveBeenCalledWith(attacker.name + ' lose battle against ' + defender.name)
+
+                        //Defender lose 2 (first attack) + (attacker health)/2 round up *2
+                        expect(defender.health).toBe(defenderHealth - 2 * Math.ceil(attackerHealth / 2))
+
+                        //Attacker lose his (health)/2 round up *2 (=0 if initial health is even -1 if not)
+                        expect(attacker.health).toBe(attackerHealth - 2 * Math.ceil(attackerHealth / 2))
+
+                    } else if (!defender.isAlive) { //attacker won
+                        expect(console.log).toHaveBeenCalledWith(defender.name + ' lose battle against ' + attacker.name)
+                        expect(defender.health).toBe(defenderHealth - 2 * Math.ceil(defenderHealth / 2))
+
+                        //Using math.floor here because attacker hit first and defender surrend before hiting back last attack
+                        expect(attacker.health).toBe(attackerHealth - 2 * Math.floor(defenderHealth / 2))
+                    }
+
+                })
+    })
+
     describe('"training" fight', () => {
         it.each(
             [
-                ['Baggie The Clown', 'Chuck Norris', 5, 9999, 0, 0, -1, 9993], //defender win
-                ['Baggie The Clown', 'Chuck Norris', 5, 9999, 1, 0, 1, 9995], //attacker surrend
-                ['Lex', 'Robin', 30, 12, 0, 0, 18, 0], //attacker win
-                ['Lex', 'Robin', 30, 12, 0, 5, 24, 4], //defender surrend
+                ['Baggie The Clown', 'Chuck Norris', 5, 9999, 0, 0], //defender win
+                ['Baggie The Clown', 'Chuck Norris', 6, 9999, 1, 0], //attacker surrend
+                ['Lex', 'Robin', 30, 12, 0, 0], //attacker win
+                ['Lex', 'Robin', 31, 12, 0, 5], //defender surrend
             ])('%s fight %s',
-                (attackerName, defenderName, attackerHealth, defenderHealth, attackerSurrender,
-                    defenderSurrender, expectedAttackerHealth, expectedDefenderHealth) => {
+                (attackerName, defenderName, attackerHealth, defenderHealth, attackerSurrender, defenderSurrender) => {
                     console.log = jest.fn();
                     const attacker = new LivingThing(attackerName, 'steel', attackerHealth)
                     const defender = new LivingThing(defenderName, 'steel', defenderHealth)
 
                     attacker.fight(defender, attackerSurrender, defenderSurrender)
 
-                    expect(attacker.health).toBe(expectedAttackerHealth)
-                    expect(defender.health).toBe(expectedDefenderHealth)
 
                     if (attacker.health <= attackerSurrender) { //defender won
                         expect(console.log).toHaveBeenCalledWith(attacker.name + ' lose battle against ' + defender.name)
+
+                        expect(defender.health).toBe(defenderHealth - 2 * Math.ceil((attackerHealth - attackerSurrender) / 2))
+                        expect(attacker.health).toBe(attackerHealth - 2 * Math.ceil((attackerHealth - attackerSurrender) / 2))
+
                     } else if (defender.health <= defenderSurrender) { //attacker won
                         expect(console.log).toHaveBeenCalledWith(defender.name + ' lose battle against ' + attacker.name)
-                    }
 
-                })
-    })
-
-    describe('Happy death fight', () => {
-        it.each(
-            [
-                ['Baggie The Clown', 'Chuck Norris', 5, 9999, -1, 9993], //defender win
-                ['Lex', 'Robin', 30, 12, 18, 0], //attacker win
-            ])('%s fight %s',
-                (attackerName, defenderName, attackerHealth, defenderHealth,
-                    expectedAttackerHealth, expectedDefenderHealth) => {
-                    console.log = jest.fn();
-                    const attacker = new LivingThing(attackerName, 'steel', attackerHealth)
-                    const defender = new LivingThing(defenderName, 'steel', defenderHealth)
-
-                    attacker.fightToDeath(defender)
-
-                    expect(attacker.health).toBe(expectedAttackerHealth)
-                    expect(defender.health).toBe(expectedDefenderHealth)
-
-                    if (!attacker.isAlive) { //defender won
-                        expect(console.log).toHaveBeenCalledWith(attacker.name + ' lose battle against ' + defender.name)
-                    } else if (!defender.isAlive) { //attacker won
-                        expect(console.log).toHaveBeenCalledWith(defender.name + ' lose battle against ' + attacker.name)
-                    }
-
-                })
-    })
-
-    describe('Witcher Hunting Monster', () => {
-        it.each(
-            [
-                ['Geralt', 'Chuck Norris', 5, 9999, -1, 9993], //defender win
-                ['Gwen', 'Robin', 30, 12, 18, 0], //attacker win
-            ])('%s fight %s',
-                (WitcherName, MonsterName, attackerHealth, defenderHealth,
-                    expectedAttackerHealth, expectedDefenderHealth) => {
-                    console.log = jest.fn();
-                    const attacker = new LivingThing(WitcherName, 'steel', attackerHealth)
-                    const defender = new LivingThing(MonsterName, 'steel', defenderHealth)
-
-                    attacker.fightToDeath(defender)
-
-                    expect(attacker.health).toBe(expectedAttackerHealth)
-                    expect(defender.health).toBe(expectedDefenderHealth)
-
-                    if (!attacker.isAlive) { //defender won
-                        expect(console.log).toHaveBeenCalledWith(attacker.name + ' lose battle against ' + defender.name)
-                    } else if (!defender.isAlive) { //attacker won
-                        expect(console.log).toHaveBeenCalledWith(defender.name + ' lose battle against ' + attacker.name)
+                        expect(defender.health).toBe(defenderHealth - 2 * Math.ceil((defenderHealth - defenderSurrender) / 2))
+                        //Using math.floor here because attacker hit first and defender surrend before hiting back last attack
+                        expect(attacker.health).toBe(attackerHealth - 2 * Math.floor((defenderHealth - defenderSurrender) / 2))
                     }
 
                 })
